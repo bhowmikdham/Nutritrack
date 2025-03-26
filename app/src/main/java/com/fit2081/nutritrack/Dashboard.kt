b3a383e1588fc75029ae936442fdc0ec4f20a4ca
bhowmikdham <142963589+bhowmikdham@users.noreply.github.com>
2025-03-27 02:48:21 +1100

    Update Dashboard.kt

--- app/src/main/java/com/fit2081/nutritrack/Dashboard.kt
+++ app/src/main/java/com/fit2081/nutritrack/Dashboard.kt
@@ -1,5 +1,6 @@
 package com.fit2081.nutritrack
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.OutlinedTextField
@@ -21,8 +23,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.fit2081.nutritrack.ui.theme.NutritrackTheme
 
 class Dashboard : ComponentActivity() {
@@ -32,14 +38,29 @@ class Dashboard : ComponentActivity() {
         setContent {
             NutritrackTheme {
                     HomePage()
-                }
+
             }
         }
     }
+}
 
 
 @Preview(showBackground = true)
 @Composable
 fun HomePage() {
-    Box()
-                }
+    val context = LocalContext.current
+    val sharedPrefs = context.getSharedPreferences("my_prefs", Context.MODE_PRIVATE)
+    val user = sharedPrefs.getString( "user_id" , "") ?: "User"
+    Scaffold {
+        innerPadding ->
+        Column(
+            modifier=Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ){
+            Text("Hello,", fontSize = 25.sp, fontWeight = FontWeight.Medium, color=Color.Gray)
+            Text(user,color=Color.Black)
+        }
+    }
+}
